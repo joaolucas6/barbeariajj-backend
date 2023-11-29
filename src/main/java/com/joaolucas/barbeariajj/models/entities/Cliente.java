947956fd3f277c72275bc171e1cf329225505ce4
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-11-29 10:30:34 -0300

    feat: criando a entidade Cliente

--- src/main/java/com/joaolucas/barbeariajj/models/entities/Cliente.java
+++ src/main/java/com/joaolucas/barbeariajj/models/entities/Cliente.java
@@ -1,4 +1,26 @@
 package com.joaolucas.barbeariajj.models.entities;
 
-public class Cliente {
+import com.joaolucas.barbeariajj.models.enums.Genero;
+import com.joaolucas.barbeariajj.models.enums.Role;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@DiscriminatorValue("Cliente")
+public class Cliente extends User{
+
+    @OneToMany(mappedBy = "cliente")
+    private List<PedidoAgendamento> pedidosAgendamento = new ArrayList<>();
+    @OneToMany(mappedBy = "cliente")
+    private List<Agendamento> agendamentos = new ArrayList<>();
+    @OneToMany(mappedBy = "cliente")
+    private List<Avaliacao> avaliacoes = new ArrayList<>();
+
+    public Cliente(Long id, String nome, String sobrenome, String email, String senha, String numeroTelefone, Genero genero, String cpf, Role role) {
+        super(id, nome, sobrenome, email, senha, numeroTelefone, genero, cpf, role);
+    }
 }
