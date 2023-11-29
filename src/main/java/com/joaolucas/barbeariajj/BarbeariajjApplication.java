5de427366014c2320c79c48abe9bde1fb6b1c430
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-11-29 09:39:43 -0300

    initial commit

---
+++ src/main/java/com/joaolucas/barbeariajj/BarbeariajjApplication.java
@@ -0,0 +1,13 @@
+package com.joaolucas.barbeariajj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariajjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariajjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaolucas/barbeariajj/BarbeariajjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaolucas.barbeariajj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariajjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
