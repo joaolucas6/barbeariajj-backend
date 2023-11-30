5c2494f58d9f42831ad25da9bd93ad56e5e94128
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-11-30 10:02:24 -0300

    feat: criando PedidoAgendamentoRepository

---
+++ src/main/java/com/joaolucas/barbeariajj/repositories/PedidoAgendamentoRepository.java
@@ -0,0 +1,7 @@
+package com.joaolucas.barbeariajj.repositories;
+
+import com.joaolucas.barbeariajj.models.entities.PedidoAgendamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoAgendamentoRepository extends JpaRepository<PedidoAgendamento, Long> {
+}
