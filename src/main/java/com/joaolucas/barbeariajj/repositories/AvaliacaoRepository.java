81015f34e640b04e5dbfe31fd28929f6cfa40a67
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-11-30 10:03:02 -0300

    feat: criando AvaliacaoRepository

---
+++ src/main/java/com/joaolucas/barbeariajj/repositories/AvaliacaoRepository.java
@@ -0,0 +1,7 @@
+package com.joaolucas.barbeariajj.repositories;
+
+import com.joaolucas.barbeariajj.models.entities.Avaliacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
+}
