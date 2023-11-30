b0aa4c0043d8ca1695f37ca9e48f86eb17a2db60
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-11-30 10:01:39 -0300

    feat: criando ServicoRepository

---
+++ src/main/java/com/joaolucas/barbeariajj/repositories/ServicoRepository.java
@@ -0,0 +1,7 @@
+package com.joaolucas.barbeariajj.repositories;
+
+import com.joaolucas.barbeariajj.models.entities.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoRepository extends JpaRepository<Servico, Long> {
+}
