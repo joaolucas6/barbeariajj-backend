dce437dcd904865ce7acee4dbe58cf0b41446fe4
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-12-01 16:06:59 -0300

    feat: criando ClienteService

---
+++ src/main/java/com/joaolucas/barbeariajj/services/ClienteService.java
@@ -0,0 +1,45 @@
+package com.joaolucas.barbeariajj.services;
+
+import com.joaolucas.barbeariajj.models.dto.ClienteDTO;
+import com.joaolucas.barbeariajj.models.entities.Cliente;
+import com.joaolucas.barbeariajj.repositories.ClienteRepository;
+import com.joaolucas.barbeariajj.utils.ValidacaoDeDados;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    public List<ClienteDTO> encontrarTodos(){
+        return clienteRepository.findAll().stream().map(ClienteDTO::new).toList();
+    }
+
+    public ClienteDTO encontrarPorId(Long id){
+        return new ClienteDTO(clienteRepository.findById(id).orElseThrow());
+    }
+
+    public ClienteDTO atualizar(Long id, ClienteDTO clienteDTO){
+        if(!ValidacaoDeDados.userValido(clienteDTO)) throw new RuntimeException();
+
+        Cliente cliente = clienteRepository.findById(id).orElseThrow();
+
+        if(clienteDTO.getNome() != null) cliente.setNome(clienteDTO.getNome());
+        if(clienteDTO.getSobrenome() != null) cliente.setSobrenome(clienteDTO.getSobrenome());
+        if(clienteDTO.getEmail() != null) cliente.setEmail(clienteDTO.getEmail());
+        if(clienteDTO.getNumeroTelefone() != null) cliente.setNumeroTelefone(clienteDTO.getNumeroTelefone());
+        if(clienteDTO.getGenero() != null) cliente.setGenero(clienteDTO.getGenero());
+        if(clienteDTO.getCpf() != null) cliente.setCpf(clienteDTO.getCpf());
+
+        return new ClienteDTO(clienteRepository.save(cliente));
+    }
+
+    public void deletar(Long id){
+        Cliente cliente = clienteRepository.findById(id).orElseThrow();
+        clienteRepository.delete(cliente);
+    }
+}
