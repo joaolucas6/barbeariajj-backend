61f3b44101e396903b456c2f6042d084e241ced8
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-12-07 15:35:55 -0300

    feat: criando AdminController

---
+++ src/main/java/com/joaolucas/barbeariajj/controllers/AdminController.java
@@ -0,0 +1,39 @@
+package com.joaolucas.barbeariajj.controllers;
+
+import com.joaolucas.barbeariajj.models.dto.AdminDTO;
+import com.joaolucas.barbeariajj.services.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/admins")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @GetMapping
+    public ResponseEntity<List<AdminDTO>> encontrarTodos(){
+        return ResponseEntity.ok(adminService.encontrarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AdminDTO> encontrarPorId(@PathVariable Long id){
+        return ResponseEntity.ok(adminService.encontrarPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AdminDTO> atualizar(@PathVariable Long id, @RequestBody AdminDTO adminDTO){
+        return ResponseEntity.ok(adminService.atualizar(id, adminDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        adminService.deletar(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
