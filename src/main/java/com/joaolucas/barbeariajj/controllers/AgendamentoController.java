bf3acd0eff1b55d428645f277ce9dc598adcb86f
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-12-07 15:30:55 -0300

    feat: criando AgendmentoController

---
+++ src/main/java/com/joaolucas/barbeariajj/controllers/AgendamentoController.java
@@ -0,0 +1,39 @@
+package com.joaolucas.barbeariajj.controllers;
+
+import com.joaolucas.barbeariajj.models.dto.AgendamentoDTO;
+import com.joaolucas.barbeariajj.services.AgendamentoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/v1/agendamentos")
+@RequiredArgsConstructor
+@RestController
+public class AgendamentoController {
+
+    private final AgendamentoService agendamentoService;
+
+    @GetMapping
+    public ResponseEntity<List<AgendamentoDTO>> encontrarTodos(){
+        return ResponseEntity.ok(agendamentoService.encontrarTodos());
+    }
+
+    @GetMapping
+    public ResponseEntity<AgendamentoDTO> encontrarPorId(@PathVariable Long id){
+        return ResponseEntity.ok(agendamentoService.encontrarPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AgendamentoDTO> atualizar(@PathVariable Long id, @RequestBody AgendamentoDTO agendamentoDTO){
+        return ResponseEntity.ok(agendamentoService.atualizar(id, agendamentoDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        agendamentoService.deletar(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
