14d303c9ceb0e8c8674da88291cf011ec2022a88
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-12-11 15:12:04 -0300

    fix: adicionando bean de clock

---
+++ src/main/java/com/joaolucas/barbeariajj/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.joaolucas.barbeariajj.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Clock clock(){
+        return Clock.systemDefaultZone();
+    }
+
+}
